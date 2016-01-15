6a33263a50cde9ede2ce2e0b9af9506394d6f19a
benjaminpettus <benjaminpettus@gmail.com>
2016-01-14 22:08:24 -1000

    completed denormalized database

---
+++ denormal_cars.sql
@@ -0,0 +1,25 @@
+CREATE USER denormal_user;
+
+CREATE DATABASE denormal_cars OWNER denormal_user;
+
+-- SELECT *
+-- FROM car_models;
+
+-- SELECT DISTINCT make_title
+-- FROM car_models
+-- ORDER BY make_title ASC;
+
+-- SELECT DISTINCT model_title
+-- FROM car_models
+-- WHERE make_code LIKE '%VOLKS%';
+
+-- SELECT make_code, model_code, model_title, year
+-- FROM car_models
+-- WHERE make_code LIKE '%LAM%';
+
+-- SELECT *
+-- FROM car_models
+-- WHERE year BETWEEN 2010 AND 2015;
+
+
+
