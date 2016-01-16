484ba58c6d9784bf540e4265de571e181079b3cc
benjaminpettus <benjaminpettus@gmail.com>
2016-01-16 11:45:50 -1000

    finished normalizing tables, working on queries

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -22,38 +22,4 @@ CREATE DATABASE denormal_cars OWNER denormal_user;
 -- WHERE year BETWEEN 2010 AND 2015;
 
 
--- CREATE TABLE year (
--- ID SERIAL PRIMARY KEY NOT NULL,
--- year = car_models(year)
--- );
-
--- CREATE TABLE make_code (
--- ID SERIAL PRIMARY KEY NOT NULL,
--- make_code character varying(50), 
--- make_title_id INT references make_title(ID),
--- year_id INT references year(ID)
--- );
-
--- CREATE TABLE make_title (
--- ID SERIAL PRIMARY KEY NOT NULL,
--- make_title character varying(20) DEFAULT NULL,
--- model_code_id INT references model_code(ID),
--- year_id INT references year(ID)
--- );
-
--- CREATE TABLE model_code (
--- ID SERIAL PRIMARY KEY NOT NULL,
--- model_code character varying(50) DEFAULT NULL,
--- model_title_id INT references model_title(ID),
--- year_id INT references year(ID)
--- );
-
--- CREATE TABLE model_title (
--- ID SERIAL PRIMARY KEY NOT NULL,
--- model_title character varying(50),
--- make_code_id INT references make_code(ID),
--- model_code_id INT references model_code(ID),
--- year_id INT references year(ID)
--- );
-
 
--- normal_cars.sql
+++ normal_cars.sql
@@ -1,39 +1,53 @@
 CREATE USER normal_user;
 
-CREATE DATABASE normal_cars OWNED normal_user;
+CREATE DATABASE normal_cars OWNER normal_user;
 
-CREATE TABLE year (
+DROP TABLE model;
+DROP TABLE make;
+DROP TABLE model_year;
+
+CREATE TABLE model_year (
 ID SERIAL PRIMARY KEY NOT NULL,
-year INT DEFAULT NOT NULL
+year INT NOT NULL
 );
 
-CREATE TABLE make_code (
+INSERT INTO model_year (year)
+SELECT DISTINCT year
+FROM car_models
+ORDER BY year ASC;
+
+CREATE TABLE make (  
 ID SERIAL PRIMARY KEY NOT NULL,
-make_code character varying(50), 
-make_title_id INT references make_title(ID),
-year_id INT references year(ID)
+make_code character varying(20) DEFAULT NULL, 
+make_title character varying(20) DEFAULT NULL
 );
 
-CREATE TABLE make_title (
+INSERT INTO make (make_code, make_title)
+SELECT DISTINCT make_code, make_title
+FROM car_models
+ORDER BY make_code ASC;
+
+
+
+CREATE TABLE model (
 ID SERIAL PRIMARY KEY NOT NULL,
-make_title character varying(20) DEFAULT NULL,
-model_code_id INT references model_code(ID),
-year_id INT references year(ID)
+model_code character varying(20) DEFAULT NULL,
+model_title character varying(45) DEFAULT NULL,
+make_id INT references make(ID),
+year_id INT references model_year(ID)
 );
 
-CREATE TABLE model_code (
-ID SERIAL PRIMARY KEY NOT NULL,
-model_code character varying(50) DEFAULT NULL,
-model_title_id INT references model_title(ID),
-year_id INT references year(ID)
-);
+INSERT INTO model (model_code, model_title, make_id, year_id)
+SELECT DISTINCT model_code, model_title, make.id, model_year.id
+FROM car_models
+INNER JOIN make ON (car_models.make_code = make.make_code)
+JOIN model_year ON (car_models.year = model_year.year);
 
-CREATE TABLE model_title (
-ID SERIAL PRIMARY KEY NOT NULL,
-model_title character varying(50),
-make_code_id INT references make_code(ID),
-model_code_id INT references model_code(ID),
-year_id INT references year(ID)
-);
+SELECT *  
+FROM model;
+
+
+
+
 
 
