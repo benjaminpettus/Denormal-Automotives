ef56c652592fb10bb9e13ef2dd5e3e7dc59643e0
benjaminpettus <benjaminpettus@gmail.com>
2016-01-16 02:11:32 -1000

    creating normalized cars relations

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -14,12 +14,46 @@ CREATE DATABASE denormal_cars OWNER denormal_user;
 -- WHERE make_code LIKE '%VOLKS%';
 
 -- SELECT make_code, model_code, model_title, year
--- FROM car_models
+-- FROM car_models[]
 -- WHERE make_code LIKE '%LAM%';
 
--- SELECT *
--- FROM car_models
--- WHERE year BETWEEN 2010 AND 2015;
+SELECT *
+FROM car_models
+WHERE year BETWEEN 2010 AND 2015;
 
+
+-- CREATE TABLE year (
+-- ID SERIAL PRIMARY KEY NOT NULL,
+-- year INT DEFAULT NOT NULL
+-- );
+
+-- CREATE TABLE make_code (
+-- ID SERIAL PRIMARY KEY NOT NULL,
+-- make_code character varying(50), 
+-- make_title_id INT references make_title(ID),
+-- year_id INT references year(ID)
+-- );
+
+-- CREATE TABLE make_title (
+-- ID SERIAL PRIMARY KEY NOT NULL,
+-- make_title character varying(20) DEFAULT NULL,
+-- model_code_id INT references model_code(ID),
+-- year_id INT references year(ID)
+-- );
+
+-- CREATE TABLE model_code (
+-- ID SERIAL PRIMARY KEY NOT NULL,
+-- model_code character varying(50) DEFAULT NULL,
+-- model_title_id INT references model_title(ID),
+-- year_id INT references year(ID)
+-- );
+
+-- CREATE TABLE model_title (
+-- ID SERIAL PRIMARY KEY NOT NULL,
+-- model_title character varying(50),
+-- make_code_id INT references make_code(ID),
+-- model_code_id INT references model_code(ID),
+-- year_id INT references year(ID)
+-- );
 
 
